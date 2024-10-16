dc4577f746ea08855de48704d932f95eda73183e
diego0-8 <laradiego.199922@gmail.com>
2024-10-16 09:59:54 -0500

    commit1

---
+++ WEBSITE1/eje 1.php
@@ -0,0 +1,15 @@
+<?php
+//este un comentario de un aline
+// escribe un mensaje
+echo "hola mundo"; // mi primer script
+
+/* comentarios e
+varias 
+lines */
+
+?>
+<h1> hola mundo <1h>
+<script>
+    alert("hola mundo")
+    </script>
+    
